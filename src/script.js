94aa5bad43fa10ca551ecd18fb91dc9dd88ebd79
Henna Anttonen <hanttone@gmail.com>
2020-10-15 11:35:52 +0200

    fixed buttons of show answer and minimize answer

--- src/script.js
+++ src/script.js
@@ -1,12 +1,27 @@
-const bookmarkIcon = document.querySelectorAll(".questioncard__bookmarkicon");
-const answerButtonShow = document.querySelectorAll(".questioncard__solution");
-const answerShow = document.querySelector(".questioncard__answer");
+const bookmarkIcon = getAll(".questioncard__bookmarkicon");
+const questionCard = getAll(".questioncard");
 
 bookmarkIcon.forEach((icon) => {
     icon.addEventListener('click', () => 
     icon.classList.toggle("questioncard__bookmarkicon--chosen"))
 });
-answerButtonShow.forEach((button) => {
-    button.addEventListener('click', () => 
-    answerShow.classList.toggle("questioncard__answer--show"))
+
+questionCard.forEach(addToggleLogic)
+
+function addToggleLogic (card) {
+  const button = card.querySelector('.questioncard__solution')
+  const answerShow = card.querySelector('.questioncard__answertext')
+  button.addEventListener('click', () => {
+      answerShow.classList.toggle("hidden");
+      if (button.innerHTML === "Show Answer") {
+          button.innerHTML = "Minimize Answer";
+      } else {
+          button.innerHTML = "Show Answer"
+      }
 });
+}
+
+function getAll (selector) {
+  return document.querySelectorAll(selector)
+}
+
