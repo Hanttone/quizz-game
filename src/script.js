00f9ae1f4c513f2de60c9b21e34341f87ba7a504
Henna Anttonen <hanttone@gmail.com>
2020-10-15 15:56:03 +0200

    made a one pager

--- src/script.js
+++ src/script.js
@@ -1,5 +1,13 @@
 const bookmarkIcon = getAll(".questioncard__bookmarkicon");
 const questionCard = getAll(".questioncard");
+const pageHome = get('[data-view="home"]');
+const pageBook = get('[data-view="bookmark"]');
+const pageCreate = get('[data-view="create"]');
+const pageProfile = get('[data-view="profile"]');
+const homeButton = get(".footer__home");
+const bookmarkButton = get(".footer__bookmark");
+const createButton = get(".footer__create");
+const profileButton = get(".footer__profile");
 
 bookmarkIcon.forEach((icon) => {
     icon.addEventListener('click', () => 
@@ -21,7 +29,39 @@ function addToggleLogic (card) {
 });
 }
 
+homeButton.addEventListener('click', () => {
+    pageHome.classList.remove("hidden");
+    pageBook.classList.add("hidden");
+    pageCreate.classList.add("hidden");
+    pageProfile.classList.add("hidden");
+})
+
+bookmarkButton.addEventListener('click', () => {
+    pageHome.classList.add("hidden");
+    pageBook.classList.remove("hidden");
+    pageCreate.classList.add("hidden");
+    pageProfile.classList.add("hidden");
+})
+
+createButton.addEventListener('click', () => {
+    pageHome.classList.add("hidden");
+    pageBook.classList.add("hidden");
+    pageCreate.classList.remove("hidden");
+    pageProfile.classList.add("hidden");
+})
+
+profileButton.addEventListener('click', () => {
+    pageHome.classList.add("hidden");
+    pageBook.classList.add("hidden");
+    pageCreate.classList.add("hidden");
+    pageProfile.classList.remove("hidden");
+})
+
 function getAll (selector) {
   return document.querySelectorAll(selector)
+}
+
+function get (selector) {
+    return document.querySelector(selector)
 }
 
